d064ac5cc0f2e25b155a044f47032c5aa7d5e4a6
TanJie <798750509@qq.com>
2020-03-07 01:50:02 +0800

    Download images

--- bin/download_end_circle2.sh
+++ bin/download_end_circle2.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 for i in {1..100}
 do
-curl -o ../static/end_circle2/$i.png https://static.ws.126.net/163/f2e/news/one_hundred/static/end_circle2/$i.png
+you-get -o ../static/end_circle2 https://static.ws.126.net/163/f2e/news/one_hundred/static/end_circle2/$i.png
 done
